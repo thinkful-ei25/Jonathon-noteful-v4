fe7a729083243d0a49be7a5a9fdc061b3332a0b7
Reldalk <Reldalk@gmail.com>
2018-10-29 16:25:43 -0400

    first day objectives complete

---
+++ routes/users.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+
+router.post('/', (req, res, next) => {
+  const { fullname, username, password } = req.body;
+  const requiredFields = ['fullname', 'username', 'password'];
+ 
+
+  let map = {};
+  for(let fields in req.body){
+    map[fields] = 1;
+  }
+  let errorMessage = undefined;
+  for(let fields in requiredFields){
+    let field = requiredFields[fields];
+    if(map[field] !== 1){
+      errorMessage === undefined ? errorMessage = field : errorMessage += ', '  + field;
+      console.log(errorMessage);
+    }
+  }
+  if(errorMessage !== undefined) {
+    const err = new Error(` '${errorMessage}' fields can not be empty`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const checkIfStringFields = requiredFields.find(field => field in req.body && typeof req.body[field] !== 'string');
+
+  if (checkIfStringFields) {
+    const err = new Error(` '${checkIfStringFields}' must be a string`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const checkIfTrimmed = requiredFields.find(field => req.body[field].trim() !== req.body[field]);
+
+  if (checkIfTrimmed) {
+    const err = new Error(` '${checkIfTrimmed} can not have leading or trailing white spaces'`);
+    err.status = 422;
+    return next(err);
+  }
+
+
+  
+  if (username.length < 2){
+    const err = new Error('username must be at least 2 characters long');
+    err.status = 422;
+    return next(err);
+  }
+
+  const passwordLength = password.length;
+  if (passwordLength < 8 || passwordLength > 72){
+    const err = new Error('password must be between 8 and 72 characters long');
+    err.status = 422;
+    return next(err);
+  }
+
+  
+    
+  return User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username,
+        password: digest,
+        fullname
+      };
+      return User.create(newUser);
+    })
+    .then(result => {
+      return res.status(201).location(`/api/users/${result.id}`).json(result);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,16 +3,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const localStrategy = require('./passport/local');
 
 const { PORT, MONGODB_URI } = require('./config');
 
 const notesRouter = require('./routes/notes');
 const foldersRouter = require('./routes/folders');
 const tagsRouter = require('./routes/tags');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 // Create an Express application
 const app = express();
 
+passport.use(localStrategy);
 // Log all requests. Skip logging during
 app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
   skip: () => process.env.NODE_ENV === 'test'
@@ -25,9 +30,11 @@ app.use(express.static('public'));
 app.use(express.json());
 
 // Mount routers
+app.use('/api/login', authRouter);
 app.use('/api/notes', notesRouter);
 app.use('/api/folders', foldersRouter);
 app.use('/api/tags', tagsRouter);
+app.use('/api/users', usersRouter);
 
 // Custom 404 Not Found route handler
 app.use((req, res, next) => {
